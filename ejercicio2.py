5bc9b611a0f0eab26d0153a0348f074ac492260f
Schoens12 <107449964+Schoens12@users.noreply.github.com>
2022-06-14 01:28:42 -0500

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,40 @@
+print('Ejercicio 2, sorteo\na)')
+
+x = rd.randint(1,5)
+
+if x == 1:
+    print('El resultado fue blanco')
+
+elif x==2:
+    print('El resultado fue negro')
+
+elif x==3:
+    print('El resultado fue rojo')
+
+elif x==4:
+    print('El resultado fue azul')
+
+elif x==5:
+    print('El resultado fue verde')
+
+print('b)')
+
+for i in range(5):
+
+    x = rd.randint(1,5)
+
+    if x == 1:
+
+        print('El resultado fue blanco')
+    
+    elif x==2:
+        print('El resultado fue negro')
+    
+    elif x==3:
+        print('El resultado fue rojo')
+
+    elif x==4:
+        print('El resultado fue azul')
+
+    elif x==5:
+        print('El resultado fue verde'
