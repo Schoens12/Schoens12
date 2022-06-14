5a6f63a62aec5d7f7ca7ff0978c1dd42829a8ea9
Schoens12 <107449964+Schoens12@users.noreply.github.com>
2022-06-14 01:29:13 -0500

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -12,3 +12,16 @@ else:
 
 print('b) ')
 
+s=0
+a=0
+
+for i in range(1000):
+
+    x = rd.random()
+
+    if x < .5:
+        s+=1
+    else:
+        a+=1
+
+print('La probabilidad de que caiga sol en un volado es de ' + str((s/1000)*100) + '%')
