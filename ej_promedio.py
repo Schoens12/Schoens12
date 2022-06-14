d87f2265ba30321768b7d11223749df8c4f064f8
Schoens12 <107449964+Schoens12@users.noreply.github.com>
2022-06-14 01:48:48 -0500

    Create ej_promedio.py

---
+++ ej_promedio.py
@@ -0,0 +1,7 @@
+a = [1, 2, 3, 4, 5] 
+b = [6,7,8,9,10] 
+ave_a = sum(a)/len(a) 
+ave_b = sum(b)/len(b) 
+
+print ("el promedio de la lista 'a' es: ", ave_a)
+print ("el promedio de la lista 'b' es: ", ave_b)
